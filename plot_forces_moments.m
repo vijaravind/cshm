633d2f5578184cf6fd1349bd703cb09fead7db2e
aravind <vijaravind@gmail.com>
2014-01-19 20:21:50 +0100

    Modify to save plots

--- plot_forces_moments.m
+++ plot_forces_moments.m
@@ -1,7 +1,8 @@
 function [] = plot_forces_moments(F, M, filename, double)
     
     % read and store force plate readings
-    filename = strcat(filename, '.csv');
+    origfile = filename;
+    filename = strcat(origfile, '.csv');
     force_plate = csvread(filename, 1, 1);
     fpT  = force_plate(:, 1);
     fpFx = force_plate(:, 2);
@@ -28,31 +29,45 @@ function [] = plot_forces_moments(F, M, filename, double)
     figure(1); hold on;
     subplot(2,1,1); plot(fpT, fpFx, 'b'); title('force plate Fx'); hold on;
     subplot(2,1,2); plot(calcT, foot_f(1,:), 'g'); title('calculated');
+    plotFx = strcat(origfile, 'Fx', '.png');
     
     % plot y forces
     figure(2); hold on;
     subplot(2,1,1); plot(fpT, fpFy, 'b'); title('force plate Fy'); hold on;
     subplot(2,1,2); plot(calcT, foot_f(2,:), 'g'); title('calculated');
+    plotFy = strcat(origfile, 'Fy', '.png');
     
     % plot z forces
     figure(3); hold on;
     subplot(2,1,1); plot(fpT, fpFz, 'b'); title('force plate Fz'); hold on;
     subplot(2,1,2); plot(calcT, -foot_f(3,:), 'g'); title('calculated');
+    plotFz = strcat(origfile, 'Fz', '.png');
     
     % plot x moments
     figure(4); hold on;
     subplot(2,1,1); plot(fpT, fpMx, 'b'); title('force plate Mx'); hold on;
     subplot(2,1,2); plot(calcT, foot_m(1,:), 'g'); title('calculated');
+    plotMx = strcat(origfile, 'Mx', '.png');
     
     % plot y moments
     figure(5); hold on;
     subplot(2,1,1); plot(fpT, fpMy, 'b'); title('force plate My'); hold on;
     subplot(2,1,2); plot(calcT, foot_m(2,:), 'g'); title('calculated');
+    plotMy = strcat(origfile, 'My', '.png');
     
     % plot z moments
     figure(6); hold on;
     subplot(2,1,1); plot(fpT, fpMz, 'b'); title('force plate Mz'); hold on;
     subplot(2,1,2); plot(calcT, foot_m(3,:), 'g'); title('calculated');
+    plotMz = strcat(origfile, 'Mz', '.png');
+    
+    % save plots
+    save2png(figure(1), plotFx);
+    save2png(figure(2), plotFy);
+    save2png(figure(3), plotFz);
+    save2png(figure(4), plotMx);
+    save2png(figure(5), plotMy);
+    save2png(figure(6), plotMz);
     
 end
 
